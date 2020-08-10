590db6c4f573af4d3f55c161074033997e88cc6e
Antti <Antti.seina@hotmail.com>
2020-08-10 15:22:43 +0300

    Jatkoa

--- site.js
+++ site.js
@@ -39,12 +39,12 @@ function PalautaJokatLista(){
 // Tästä alkaa osalistat
 
 var juoksuLista = [
-    {haku: "4m juoksu", nimi: "Juoksu 4,14 m, teräs LW", pituus: 4.14, paino: 13.4},
-    {haku: "3m juoksu", nimi: "Juoksu 3,07 m, teräs LW", pituus: 3.07, paino: 10.1},
-    {haku: "2,5m juoksu", nimi: "Juoksu 2,57 m, teräs LW", pituus: 2.57, paino: 8.5},
-    {haku: "2m juoksu", nimi: "Juoksu 2,07 m, teräs LW", pituus: 2.07, paino: 7.0},
-    {haku: "1,5m juoksu", nimi: "Juoksu 1,57 m, teräs LW", pituus: 1.57, paino: 5.5}, 
-    {haku: "1,4 juoksu", nimi: "Juoksu 1,57 m, teräs LW", pituus: 1.40, paino: 5.0}
+    {haku: "4m juoksu", nimi: "Juoksu 4,14 m, teräs LW", pituus: 4.09, paino: 13.4},
+    {haku: "3m juoksu", nimi: "Juoksu 3,07 m, teräs LW", pituus: 3.02, paino: 10.1},
+    {haku: "2,5m juoksu", nimi: "Juoksu 2,57 m, teräs LW", pituus: 2.52, paino: 8.5},
+    {haku: "2m juoksu", nimi: "Juoksu 2,07 m, teräs LW", pituus: 2.02, paino: 7.0},
+    {haku: "1,5m juoksu", nimi: "Juoksu 1,57 m, teräs LW", pituus: 1.52, paino: 5.5}, 
+    {haku: "1,4 juoksu", nimi: "Juoksu 1,57 m, teräs LW", pituus: 1.35, paino: 5.0}
 ];
 var jokatLista = [
     {haku: "3m juoksuvahvari", nimi: "U-kaksoisjuoksu 3,07 m, teräs LW", pituus: 3.07, paino: 19.0},
