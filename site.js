ee8218616588be2630e491335796d5d2219e1b7b
Antti <Antti.seina@hotmail.com>
2020-08-12 14:37:41 +0300

    Massalaskuri toimii!

--- site.js
+++ site.js
@@ -17,66 +17,88 @@ function Alkuun() {
 
 //Osalistanapit
 
-function PalautaJuoksuLista(){
+function PalautaJuoksuLista() {
     document.getElementById("osat").innerHTML = "";
     for (let index = 0; index < juoksuLista.length; index++) {
         var element = juoksuLista[index];
-        document.getElementById("osat").innerHTML += "<li>" + element.nimi + ": Oikea pituus: " +
-        element.pituus.toPrecision(3) + "m, Massa: " + element.paino.toPrecision(3) + "kg" + "</li";   
-    };  
+        if (element.nimi.length) {
+            document.getElementById("osat").innerHTML += "<li>" + element.nimi + ": Oikea pituus: " +
+                element.pituus.toPrecision(3) + "m, Massa: " + element.paino.toPrecision(3) + "kg" + "</li>";
+
+        }
+    }
 };
-function PalautaJokatLista(){
+function PalautaJokatLista() {
     document.getElementById("osat").innerHTML = "";
     for (let index = 0; index < jokatLista.length; index++) {
         var element = jokatLista[index];
-        if (element.nimi.length){
+        if (element.nimi.length) {
+            document.getElementById("osat").innerHTML += "<li>" + element.nimi + ": Oikea pituus: " +
+                element.pituus.toPrecision(3) + "m, Massa: " + element.paino.toPrecision(3) + "kg" + "</li>";
 
         }
-        document.getElementById("osat").innerHTML += "<li>" + element.nimi + ": Oikea pituus: " +
-        
-        element.pituus.toPrecision(3) + "m, Massa: " + element.paino.toPrecision(3) + "kg" + "</li>";   
-    };  
+
+    }
 };
+
 //Massalaskuri
 
 var osienLisaysKentta = "";
 var osienMaaraKentta = 0;
 var kokonaisMassa = 0;
 
-function LisaaMassaa(){
-    osienLisaysKentta = document.getElementById("massanlisäyssyöte").value;
-    osienMaaraKentta = document.getElementById("massanmääräsyöte").value;
-    const lisaaOsaa = "";
-    const lisaaMassaa = 0; 
-    for (let i = 0; i < juoksuLista.length; i++) {
-        const objekti = juoksuLista[i];
-        if(osienLisaysKentta == objekti.haku){
-            document.getElementById("massalista").innerHTML += "<li>" + objekti.nimi + ": Oikea pituus: " +
-            objekti.pituus.toPrecision(3) + "m, Massa: " + objekti.paino.toPrecision(3) + "kg | " + OsienMaaraKentta + "kpl" + "</li>";
-            kokonaisMassa+=objekti.paino*osienMaaraKentta;
-            document.getElementById("summapaikka").innerHTML=kokonaisMassa;
-        } 
-    };
+function TyhjennaTaulu() {
+    document.getElementById('massalista').innerHTML = '';
+    document.getElementById('summapaikka').innerHTML = '0kg';
+
 };
 
+
+function LisaaMassaa() {
+    osienLisaysKentta = document.getElementById("massanlisäyssyöte").value;
+    osienMaaraKentta = document.getElementById("massanmääräsyöte").value;
+
+    for (let index = 0; index < juoksuLista.length; index++) {
+        const element = juoksuLista[index];
+        if (element.haku == osienLisaysKentta) {
+            kokonaisMassa += osienMaaraKentta * element.paino;
+            document.getElementById("summapaikka").innerHTML = kokonaisMassa + "kg";
+            document.getElementById("massalista").innerHTML += "<li>" + osienMaaraKentta + " kpl " +
+                element.nimi + "- Oikea pituus: " + element.pituus + ", Osan massa: " + element.paino;
+
+        }
+    }
+
+    for (let index = 0; index < jokatLista.length; index++) {
+        const element = jokatLista[index];
+        if (element.haku == osienLisaysKentta) {
+            kokonaisMassa += osienMaaraKentta * element.paino;
+            document.getElementById("summapaikka").innerHTML = kokonaisMassa + "kg";
+            document.getElementById("massalista").innerHTML += "<li>" + osienMaaraKentta + " kpl " +
+                element.nimi + "- Oikea pituus: " + element.pituus + ", Osan massa: " + element.paino;
+        }
+    }
+
+
+};
 
 // Tästä alkaa osalistat
 
 var juoksuLista = [
-    {haku: "4m juoksu", nimi: "Juoksu 4,14 m, teräs LW", pituus: 4.09, paino: 13.4},
-    {haku: "3m juoksu", nimi: "Juoksu 3,07 m, teräs LW", pituus: 3.02, paino: 10.1},
-    {haku: "2,5m juoksu", nimi: "Juoksu 2,57 m, teräs LW", pituus: 2.52, paino: 8.5},
-    {haku: "2m juoksu", nimi: "Juoksu 2,07 m, teräs LW", pituus: 2.02, paino: 7.0},
-    {haku: "1,5m juoksu", nimi: "Juoksu 1,57 m, teräs LW", pituus: 1.52, paino: 5.5}, 
-    {haku: "1,4 juoksu", nimi: "Juoksu 1,57 m, teräs LW", pituus: 1.35, paino: 5.0}
+    { haku: "4m juoksu", nimi: "Juoksu 4,14 m, teräs LW", pituus: 4.09, paino: 13.4 },
+    { haku: "3m juoksu", nimi: "Juoksu 3,07 m, teräs LW", pituus: 3.02, paino: 10.1 },
+    { haku: "2,5m juoksu", nimi: "Juoksu 2,57 m, teräs LW", pituus: 2.52, paino: 8.5 },
+    { haku: "2m juoksu", nimi: "Juoksu 2,07 m, teräs LW", pituus: 2.02, paino: 7.0 },
+    { haku: "1,5m juoksu", nimi: "Juoksu 1,57 m, teräs LW", pituus: 1.52, paino: 5.5 },
+    { haku: "1,4 juoksu", nimi: "Juoksu 1,57 m, teräs LW", pituus: 1.35, paino: 5.0 }
 ];
 var jokatLista = [
-    {haku: "3m juoksuvahvari", nimi: "U-kaksoisjuoksu 3,07 m, teräs LW", pituus: 3.07, paino: 19.0},
-    {haku: "2,5m juoksuvahvari", nimi: "U-kaksoisjuoksu 2,57 m, teräs LW", pituus: 2.57, paino: 15.7},
-    {haku: "2m juoksuvahvari", nimi: "U-kaksoisjuoksu 2,07 m, teräs LW", pituus: 2.07, paino: 12.7},
-    {haku: "1,5m juoksuvahvari", nimi: "U-kaksoisjuoksu 1,57 m, teräs LW", pituus: 1.57, paino: 7.4},
-    {haku: "1m jokka", nimi: "U-jokka, vahv. 1,09 m", pituus: 1.09, paino: 5.7}, 
-    {haku: "3m vahvari", nimi: "U-jokka vahv. 3,07 m LW T14", pituus: 1.40, paino: 19.0}
+    { haku: "3m juoksuvahvari", nimi: "U-kaksoisjuoksu 3,07 m, teräs LW", pituus: 3.07, paino: 19.0 },
+    { haku: "2,5m juoksuvahvari", nimi: "U-kaksoisjuoksu 2,57 m, teräs LW", pituus: 2.57, paino: 15.7 },
+    { haku: "2m juoksuvahvari", nimi: "U-kaksoisjuoksu 2,07 m, teräs LW", pituus: 2.07, paino: 12.7 },
+    { haku: "1,5m juoksuvahvari", nimi: "U-kaksoisjuoksu 1,57 m, teräs LW", pituus: 1.57, paino: 7.4 },
+    { haku: "1m jokka", nimi: "U-jokka, vahv. 1,09 m", pituus: 1.09, paino: 5.7 },
+    { haku: "3m vahvari", nimi: "U-jokka vahv. 3,07 m LW T14", pituus: 1.40, paino: 19.0 }
 ];
 
 
